54303d96946b155ee3318d2b1dfd0dbe1b27a01c
ImVeryBad <nightbear843@yahoo.com>
2026-01-21 01:15:54 +0100

    Testing with a custom wizard step

--- src/main/kotlin/com/nova/funkindevplugin/projects/VSliceNewProjectWizard.kt
+++ src/main/kotlin/com/nova/funkindevplugin/projects/VSliceNewProjectWizard.kt
@@ -1,14 +1,7 @@
 package com.nova.funkindevplugin.projects
 
-import com.intellij.ide.wizard.AbstractNewProjectWizardStep
 import com.intellij.ide.wizard.NewProjectWizardStep
 import com.intellij.ide.wizard.language.LanguageGeneratorNewProjectWizard
-import com.intellij.ide.wizard.setupProjectFromBuilder
-import com.intellij.openapi.module.GeneralModuleType
-import com.intellij.openapi.module.ModuleTypeManager
-import com.intellij.openapi.project.Project
-import com.intellij.plugins.haxe.ide.module.HaxeModuleType
-import com.nova.funkindevplugin.module.VSliceModuleType
 import icons.VSliceIcons
 import javax.swing.Icon
 
@@ -17,14 +10,5 @@ class VSliceNewProjectWizard : LanguageGeneratorNewProjectWizard {
     override val icon: Icon = VSliceIcons.VSliceIcon
     override val ordinal = 250
 
-    override fun createStep(parent: NewProjectWizardStep) = Step(parent)
-
-    class Step(parent: NewProjectWizardStep) :
-        AbstractNewProjectWizardStep(parent) {
-        override fun setupProject(project: Project) {
-            val moduleType = VSliceModuleType.INSTANCE
-            val builder = moduleType.createModuleBuilder()
-            setupProjectFromBuilder(project, builder)
-        }
-    }
+    override fun createStep(parent: NewProjectWizardStep) = VSliceNewProjectWizardStep(parent)
 }
