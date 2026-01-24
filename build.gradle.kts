5d2eedffeaa3048261fae7b390cfddd143f65d50
ImVeryBad <nightbear843@yahoo.com>
2026-01-24 19:03:24 +0100

    Added autocompletion for mod metadata.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 
         // Add necessary plugin dependencies for compilation here, example:
         bundledPlugin("com.intellij.java")
+        bundledPlugin("com.intellij.modules.json")
         bundledPlugin("com.jetbrains.sh")
         plugin("com.intellij.plugins.haxe:$htkVersion")
     }
