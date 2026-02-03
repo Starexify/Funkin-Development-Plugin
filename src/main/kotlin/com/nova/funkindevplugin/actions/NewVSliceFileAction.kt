57f257f06f76db72b9afe4f146ea389addb64b40
ImVeryBad <nightbear843@yahoo.com>
2026-02-03 17:43:45 +0100

    Now the VSlice Script File action is only visible in VSlice mods.

--- src/main/kotlin/com/nova/funkindevplugin/actions/NewVSliceFileAction.kt
+++ src/main/kotlin/com/nova/funkindevplugin/actions/NewVSliceFileAction.kt
@@ -2,8 +2,14 @@ package com.nova.funkindevplugin.actions
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.NlsContexts
+import com.intellij.plugins.haxe.ide.module.HaxeModuleType
 import com.intellij.psi.PsiDirectory
 import com.nova.funkindevplugin.VSliceBundle
 import icons.VSliceIcons
@@ -27,6 +33,28 @@ class NewVSliceFileAction :
     kinds.forEach { kind ->
       builder.addKind(kind, VSliceIcons.VSliceIcon, kind)
     }
+  }
+
+  /**
+   * Checks if the project is a haxe module and contains a Polymod metadata file.
+   *
+   * @return false if the statement above is false
+   */
+  override fun isAvailable(dataContext: DataContext): Boolean {
+    val project = CommonDataKeys.PROJECT.getData(dataContext) ?: return false
+    val viewFile = CommonDataKeys.VIRTUAL_FILE.getData(dataContext) ?: return false
+
+    val module = ModuleUtilCore.findModuleForFile(viewFile, project)
+
+    val isHaxe = module?.let { ModuleType.get(it) is HaxeModuleType } ?: false
+    if (!isHaxe) return false
+
+    val model = module?.let { ModuleRootManager.getInstance(it) }
+    val hasVSliceMarker = model?.contentRoots?.any { root ->
+      root.findChild("_polymod_meta.json") != null
+    } ?: false
+
+    return super.isAvailable(dataContext) && hasVSliceMarker
   }
 
   override fun getActionName(
